f693194d375f58418e4d3e7bd7e9b5f0c9a97d9d
ARUNAT007 <arunathiru05@gmail.com>
2026-01-20 11:57:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var popupoverlay=document.querySelector(".overlay")
+var popupbox=document.querySelector(".popup")
+var addpopupbutton=document.getElementById("add-popup-button")
+    addpopupbutton.addEventListener("click",function(){
+    popupbox.style.display="block"  
+    popupoverlay.style.display="block"  
+})
+var cancelbutton=document.getElementById("cancel-popup")
+cancelbutton.addEventListener("click",function(event){
+    event.preventDefault()
+    popupbox.style.display="none"  
+    popupoverlay.style.display="none"  
+})
+var container=document.querySelector(".container")
+var addbook=document.getElementById("add-book")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+                <h4>${bookauthorinput.value}</h4>
+                <p>${bookdescriptioninput.value}</p>
+                <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    popupbox.style.display="none"  
+    popupoverlay.style.display="none"
+
+})
+function deletebook(event){
+    event.target.parentElement.remove()
+}
